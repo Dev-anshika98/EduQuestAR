703785b0093400180bd406d405adfe4c1a4dbcbe
Dev-anshika98 <g.anshikacreative@gmail.com>
2024-01-21 00:10:32 +0530

    cards section modified

--- Webpage/src/components/Cards.jsx
+++ Webpage/src/components/Cards.jsx
@@ -24,7 +24,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-[#00435B] p-5 rounded-2xl sm:w-[360px] w-full'
+        className='bg-[#071e3d] p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
           <img
